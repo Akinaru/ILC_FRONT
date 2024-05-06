c1694235424f94d15d46d60bc4db8210f991ea57
Akinaru <maxime.utchimata@gmail.com>
2024-05-06 10:24:33 +0200

    ajout departement & composante

--- src/pages/dashboard/departement.vue
+++ src/pages/dashboard/departement.vue
@@ -4,11 +4,21 @@
         <div class="m-5">
             <div class="m-5 flex justify-center items-center flex-col">
                 <p class="text-lg font-bold">Ajout département</p>
-                <form @submit.prevent="addDepartment" class="w-2/5 *:my-2">
-                    <select class="select select-bordered w-full" v-model="newDep.comp">
+                <form @submit.prevent="addDepartment" class="w-2/5 *:my-2 flex flex-col justify-center items-center">
+                    <select class="select select-bordered w-full" v-model="newDep.compo">
                         <option disabled selected>Selectionnez une composante</option>
-                        <option v-for="(compo, index) in composantes" :key="index" :value="compo.comp_id">{{ compo.comp_name }}</option>
+                        <option v-for="(compo, index) in composantes.components" :key="index" :value="compo.comp_id">{{ compo.comp_name }}</option>
+                        <option value="addNew">Créer une composante</option>
                     </select>
+                    <label class="form-control w-5/6 my-1 flex flex-col justify-center items-center" v-if="newDep.compo === 'addNew'">
+                        <div class="label">
+                            <span class="label-text">Créer une composante</span>
+                        </div>
+                        <input type="text" placeholder="Nom (ex: IUT Annecy)" v-model="newDep.newcompo.name" class="input input-bordered w-full" />
+                        <input type="text" placeholder="Nom raccourci (ex: IUT-A)" v-model="newDep.newcompo.shortname" class="input input-bordered w-full " />
+                    </label>
+
+
                     <div class="w-full flex items-center justify-between">
                         <label for="colorPicker" class="label-text">Couleur</label> 
                         <input type="color" id="colorPicker" v-model="newDep.color">
@@ -28,22 +38,28 @@
 
         <div>
             <p class="text-lg font-bold">Liste département</p>
-            <div v-if="composantes && composantes.length > 0">
-
-                <div v-for="(compo, index) in composantes" :key="index">
-                    <li class="list-disc">{{ compo.comp_name }}</li>
-                    <div v-for="(dept, index) in compo.departments" :key="index" class="mx-10 flex m-1">
-                        
-                        <div class="bg-base-300 w-full p-5 flex items-center justify-center">
-                            <div class="h-10 w-10 mx-5" :style="{backgroundColor: dept.dept_color}"></div>
-                            <p class="w-full">{{ dept.dept_name }} ({{ dept.dept_shortname }})</p>
+            <div v-if="composantes && composantes.components">
+                <div v-if="composantes.count > 0">
+                    
+                    <div v-for="(compo, index) in composantes.components" :key="index">
+                        <li class="list-disc">{{ compo.comp_name }}</li>
+                        <div v-for="(dept, index) in compo.departments" :key="index" class="mx-10 flex m-1">
+                            
+                            <div class="bg-base-300 w-full p-5 flex items-center justify-center">
+                                <div class="h-10 w-10 mx-5" :style="{backgroundColor: dept.dept_color}"></div>
+                                <p class="w-full">{{ dept.dept_name }} ({{ dept.dept_shortname }})</p>
+                            </div>
+                            <button class="hover:opacity-60 hover:cursor-pointer bg-base-300 flex items-center justify-center p-5" @click="removeDepartment(dept.dept_id)">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-5 w-5" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path></svg>
+                            </button>
                         </div>
-                        <button class="hover:opacity-60 hover:cursor-pointer bg-base-300 flex items-center justify-center p-5" @click="removeDepartment(dept.dept_id)">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-5 w-5" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path></svg>
-                        </button>
+                        <p class="list-disc mx-10 my-5" v-if="compo.departments.length == 0">Aucun département</p>
                     </div>
-                    <p class="list-disc mx-10 my-5" v-if="compo.departments.length == 0">Aucun département</p>
                 </div>
+                <div v-else>
+                    <p>Aucune composante trouvée.</p>
+                </div>
+
             </div>
             <div v-else class="flex items-center justify-center my-20">
                 <span class="loading loading-dots loading-lg"></span>
@@ -58,11 +74,19 @@
     import { request } from '../../composables/httpRequest';
     import config from '../../config';
 
-    const departements = ref([]);
     const composantes = ref([]);
     const response = ref([]);
 
-    const newDep = ref({ name: '', shortname: '', color: '#9e9e9e', compo: 0 });
+    const newDep = ref({ 
+        name: '', 
+        shortname: '', 
+        color: '#9e9e9e', 
+        compo: '', 
+        newcompo: {
+            name: '', 
+            shortname: ''
+        } 
+    });
 
 
     async function addDepartment(){
@@ -70,8 +94,17 @@
             dept_name: newDep.value.name,
             dept_shortname: newDep.value.shortname.toUpperCase(),
             dept_color: newDep.value.color,
-            comp_id: newDep.value.comp 
         };
+        // Gestion de la nouvelle composante
+        if (newDep.value.compo !== 'addNew') {
+            requestData.comp_id = newDep.value.compo;
+        } else {
+            requestData.newcompo = {
+                comp_name: newDep.value.newcompo.name,
+                comp_shortname: newDep.value.newcompo.shortname.toUpperCase()
+            };
+        }
+        console.log(requestData)
         await request("POST", true, response, config.apiUrl+'api/department', requestData);
         await fetchAll();
         resetInput();
@@ -84,7 +117,6 @@
     }
 
     async function fetchAll(){
-        await request('GET', false, departements, config.apiUrl+'api/department');
         await request('GET',false, composantes, config.apiUrl+'api/component');
         resetInput();
     }
