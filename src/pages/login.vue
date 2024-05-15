9c5175ef5184fdd719b0b3cdec6f6aabe1b20f6f
Akinaru <maxime.utchimata@gmail.com>
2024-05-15 10:13:15 +0200

    dept validation

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,7 +1,6 @@
 <template>
-    <div class="min-h-screen min-w-screen">
         <div class="m-5">
-            <div class="m-5 flex justify-center items-center flex-col" >
+            <div class="m-5 flex justify-center items-center flex-col pt-72" >
                 <p class="text-lg font-bold">Connexion</p>
                 <form @submit.prevent="login" class="w-fit *:my-2">
                     <input type="text" placeholder="Login" v-model="newLogin.login" class="input input-bordered w-full " />
@@ -9,37 +8,17 @@
                         <button class="btn btn-primary" type="submit">Se connecter</button>
                     </div>
                 </form>
-
-                <p class="text-lg font-bold">Recherche login</p>
-                <form @submit.prevent="searchUser" class="w-fit *:my-2">
-                    <input type="text" placeholder="Login" v-model="name" class="input input-bordered w-full " />
-                    <div class="flex items-center justify-center">
-                        <button class="btn btn-primary" type="submit">Rechercher</button>
-                    </div>
-                </form>
             </div>
         </div>
-        <div v-if="decomposedInfo">
-            <p>Full Name: {{ decomposedInfo.fullname }}</p>
-            <p>Status: {{ decomposedInfo.status }}</p>
-            <p v-if="decomposedInfo.fonction">Function: {{ decomposedInfo.fonction }}</p>
-            <p v-else>Function: N/A</p>
-        </div>
-    </div>
 </template>
 
 <script setup>
-    import { request } from '../composables/httpRequest'
-    import { onMounted, ref } from 'vue';
-    import {decomposeDN} from '../composables/destructLDAP'
+    import { ref } from 'vue';
     import { authLogAccount } from '../composables/authGuard'
     import { useRouter } from 'vue-router';
 
     const router = useRouter();
-    const newLogin = ref({ login: 'gallottm', password: '' });
-    const response = ref([]);
-    const name = ref("");
-    const decomposedInfo = ref(null);
+    const newLogin = ref({ login: 'sbouc'});
 
     const validLogin = ['gallottm', 'ldama', 'martmate', 'vcout'];
 
@@ -50,17 +29,4 @@
             console.log("Le login n'est pas valide.");
         }
     }
-
-
-    async function searchUser() {
-        if(name.value.trim() !== "") {
-            await request('GET', true, response, `http://srv-peda.iut-acy.local/ldama/ldap/?login=${name.value.trim()}`);
-            decomposedInfo.value = decomposeDN(name.value.trim(), response.value[0].dn);
-        }
-    }
-    async function fetch(){
-        name.value = 'ldama';
-        searchUser();
-    }
-    onMounted(fetch);
 </script>
