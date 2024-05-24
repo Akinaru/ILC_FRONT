ede0b4816b85c5326525001c99e1d752153a52a6
Akinaru <maxime.utchimata@gmail.com>
2024-05-24 19:45:24 +0200

    calendrier

--- src/components/utils/CalendarComp.vue
+++ src/components/utils/CalendarComp.vue
@@ -1,17 +1,27 @@
 <template>
     <div class="bg-base-300 w-fit p-5 drop-shadow-lg">
-      <div class="py-3 w-4/5 flex">
-        <select id="month" v-model="selectedMonth" class="p-2 mx-1 w-full font-bold drop-shadow-lg">
+      <div class="py-3 w-4/5 flex w-full">
+        <select id="month" v-model="selectedMonth" class="hover:opacity-60 hover:cursor-pointer select-none p-2 mx-1 w-full font-bold drop-shadow-lg">
           <option v-for="(month, index) in months" :key="index" :value="index">
             {{ month }}
           </option>
         </select>
   
-        <select id="year" v-model="selectedYear" class="p-2 mx-1 w-full font-bold drop-shadow-lg">
+        <select id="year" v-model="selectedYear" class="hover:opacity-60 hover:cursor-pointer select-none p-2 mx-1 w-full font-bold drop-shadow-lg">
           <option v-for="year in years" :key="year" :value="year">
             {{ year }}
           </option>
         </select>
+        <div class="flex items-center justify-center font-bold  w-full *:mx-2">
+          <svg @click="previousMonth()" class="select-none hover:opacity-60 hover:cursor-pointer" xmlns="http://www.w3.org/2000/svg" fill="black" width="36" height="36" viewBox="0 0 24 24">
+            <path d="M5 12l7-8v6h9v4h-9v6z"/>
+          </svg>
+
+          <svg @click="nextMonth()" class="select-none hover:opacity-60 hover:cursor-pointer" xmlns="http://www.w3.org/2000/svg" fill="black" width="36" height="36" viewBox="0 0 24 24">
+            <path d="M19 12l-7 8v-6H3v-4h9v-6z"/>
+          </svg>
+        </div>
+
       </div>
       
       <table>
@@ -23,13 +33,13 @@
         <tbody>
           <tr v-for="week in calendar" :key="week">
             <td 
-              class="p-3 lg:p-6 font-bold hover:cursor-pointer transition-opacity  select-none hover:opacity-60 hover:bg-base-200 relative" 
+              class="p-3 lg:p-6 font-bold hover:cursor-pointer transition-opacity select-none hover:bg-base-200 relative hover:drop-shadow-lg" 
               v-for="day in week" 
-              :key="day.date" 
-              :class="{ 'font-normal opacity-60 ': day.isNotMonth, 'bg-base-100 drop-shadow-lg': day.isToday }"
+              :key="day.date"
+              :class="{ 'font-normal  ': day.isNotMonth, 'bg-error drop-shadow-lg': day.isToday, 'bg-base-200 drop-shadow-lg' : dayHasEvent(day) }"
             >
               {{ day.date }}
-              <span class="badge badge-accent absolute top-0 right-0 mt-1 mr-1" v-if="dayHasEvent(day)">1</span>
+              <span class="badge badge-accent absolute top-0 right-0 mt-1 mr-1 font-bold" v-if="dayHasEvent(day)">{{ countEventsOnDay(day) }}</span>
             </td>
           </tr>
         </tbody>
@@ -38,94 +48,152 @@
   </template>
   
 <script setup>
-    import { createPinia } from 'pinia';
-import { ref, computed } from 'vue';
-    const props = defineProps({
-      events: Object,
+  import { createPinia } from 'pinia';
+  import { ref, computed } from 'vue';
+  const props = defineProps({
+    events: Object,
+  });
+
+  const months = ref([
+    'Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 
+    'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'
+  ]);
+  const days = ref(['L', 'M', 'M', 'J', 'V', 'S', 'D']);
+      
+  const currentYear = new Date().getFullYear();
+  const currentMonth = new Date().getMonth();
+  const currentDate = new Date().getDate();
+  const years = ref(Array.from({ length: 21 }, (_, i) => currentYear - 10 + i));
+
+  const selectedMonth = ref(currentMonth);
+  const selectedYear = ref(currentYear);
+
+  const daysInMonth = (year, month) => {
+    return new Date(year, month + 1, 0).getDate();
+  };
+  
+  const firstDayOfMonth = (year, month) => {
+    return (new Date(year, month, 1).getDay() + 6) % 7;
+  };
+  
+  // Retourne la liste correspondante au calendrier avec le mois et l"année séléctionné
+  const calendar = computed(() => {
+    let year = selectedYear.value;
+    let month = selectedMonth.value;
+    let numDays = daysInMonth(year, month);
+    let startDay = firstDayOfMonth(year, month);
+
+    let daysArray = [];
+    
+    // Days from previous month
+    let prevMonthDays = startDay;
+    let prevMonth = month === 0 ? 11 : month - 1;
+    let prevMonthYear = month === 0 ? year - 1 : year;
+    let prevMonthNumDays = daysInMonth(prevMonthYear, prevMonth);
+    for (let i = prevMonthNumDays - prevMonthDays + 1; i <= prevMonthNumDays; i++) {
+      daysArray.push({ date: i, isNotMonth: true, monthBefore: true, monthAfter: false, isToday: false });
+    }
+
+    // Days of the current month
+    for (let i = 1; i <= numDays; i++) {
+      let isToday = year === currentYear && month === currentMonth && i === currentDate;
+      daysArray.push({ date: i, isNotMonth: false, monthBefore: false, monthAfter: false, isToday });
+    }
+
+    // Days from next month
+    let nextMonthDays = (42 - daysArray.length);
+    for (let i = 1; i <= nextMonthDays; i++) {
+      daysArray.push({ date: i, isNotMonth: true, monthBefore: false, monthAfter: true, isToday: false });
+    }
+
+    let weeks = [];
+    while (daysArray.length > 0) {
+      weeks.push(daysArray.splice(0, 7));
+    }
+
+    return weeks;
+  });
+
+  const dayHasEvent = (day) => {
+    let year = selectedYear.value;
+    let month = selectedMonth.value + 1; // Ajoute 1 car les mois dans JavaScript sont indexés à partir de 0
+
+    // Si le jour appartient au mois précédent
+    if (day.monthBefore) {
+      month--; // Mois précédent
+      if (month === 0) {
+        month = 12; // Retourner à décembre de l'année précédente
+        year--;
+      }
+    }
+    // Si le jour appartient au mois suivant
+    else if (day.monthAfter) {
+      month++; // Mois suivant
+      if (month === 13) {
+        month = 1; // Retourner à janvier de l'année suivante
+        year++;
+      }
+    }
+
+    // Construire la date de l'événement
+    let eventDate = `${year}-${month.toString().padStart(2, '0')}-${day.date.toString().padStart(2, '0')}`;
+
+    // Vérifier si un événement se produit à cette date
+    return props.events.events.some(event => {
+      let eventDateTime = event.evt_datetime.split(' ')[0];
+      return eventDateTime === eventDate;
     });
+  };
 
-      const months = ref([
-        'Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 
-        'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'
-      ]);
-  
+  const countEventsOnDay = (day) => {
+    let year = selectedYear.value;
+    let month = selectedMonth.value + 1; // Ajoute 1 car les mois dans JavaScript sont indexés à partir de 0
 
-      
-      const days = ref(['L', 'M', 'M', 'J', 'V', 'S', 'D']);
-      
-      const currentYear = new Date().getFullYear();
-      const currentMonth = new Date().getMonth();
-      const currentDate = new Date().getDate();
-      const years = ref(Array.from({ length: 21 }, (_, i) => currentYear - 10 + i));
-  
-      const selectedMonth = ref(currentMonth);
-      const selectedYear = ref(currentYear);
-  
-      const daysInMonth = (year, month) => {
-        return new Date(year, month + 1, 0).getDate();
-      };
-  
-      const firstDayOfMonth = (year, month) => {
-        return (new Date(year, month, 1).getDay() + 6) % 7;
-      };
-  
-      const calendar = computed(() => {
-        const year = selectedYear.value;
-        const month = selectedMonth.value;
-        const numDays = daysInMonth(year, month);
-        const startDay = firstDayOfMonth(year, month);
-  
-        const daysArray = [];
-        
-        // Days from previous month
-        const prevMonthDays = startDay;
-        const prevMonth = month === 0 ? 11 : month - 1;
-        const prevMonthYear = month === 0 ? year - 1 : year;
-        const prevMonthNumDays = daysInMonth(prevMonthYear, prevMonth);
-        for (let i = prevMonthNumDays - prevMonthDays + 1; i <= prevMonthNumDays; i++) {
-          daysArray.push({ date: i, isNotMonth: true, isToday: false });
-        }
-  
-        // Days of the current month
-        for (let i = 1; i <= numDays; i++) {
-          const isToday = year === currentYear && month === currentMonth && i === currentDate;
-          daysArray.push({ date: i, isNotMonth: false, isToday });
-        }
-  
-        // Days from next month
-        const nextMonthDays = (42 - daysArray.length);
-        for (let i = 1; i <= nextMonthDays; i++) {
-          daysArray.push({ date: i, isNotMonth: true, isToday: false });
-        }
-  
-        const weeks = [];
-        while (daysArray.length > 0) {
-          weeks.push(daysArray.splice(0, 7));
-        }
-  
-        return weeks;
-      });
+    // Si le jour appartient au mois précédent
+    if (day.monthBefore) {
+      month--; // Mois précédent
+      if (month === 0) {
+        month = 12; // Retourner à décembre de l'année précédente
+        year--;
+      }
+    }
+    // Si le jour appartient au mois suivant
+    else if (day.monthAfter) {
+      month++; // Mois suivant
+      if (month === 13) {
+        month = 1; // Retourner à janvier de l'année suivante
+        year++;
+      }
+    }
 
-      const dayHasEvent = (day) => {
-        // Vérifie s'il y a des événements pour le jour donné
-        const year = selectedYear.value;
-        const month = selectedMonth.value + 1; // Ajoute 1 car les mois dans JavaScript sont indexés à partir de 0
-        const eventDate = `${year}-${month.toString().padStart(2, '0')}-${day.date.toString().padStart(2, '0')}`;
+    // Construire la date de l'événement
+    const eventDate = `${year}-${month.toString().padStart(2, '0')}-${day.date.toString().padStart(2, '0')}`;
 
-        return props.events.events.some(event => {
-          const eventDateTime = event.evt_datetime.split(' ')[0];
-          return eventDateTime === eventDate;
-        });
-      };
+    // Compter le nombre d'événements à cette date
+    return props.events.events.filter(event => {
+      const eventDateTime = event.evt_datetime.split(' ')[0];
+      return eventDateTime === eventDate;
+    }).length;
+  };
 
-const nbEventOnDay = (day) => {
-  // Retourne le nombre d'événements pour le jour donné
-  const eventDate = day.date.toString().padStart(2, '0'); // Convertit la date du jour en string
-  return props.events.events.filter(event => {
-    const eventDateTime = event.evt_datetime.split(' ')[0]; // Extrait la date de evt_datetime
-    return eventDateTime === eventDate;
-  }).length;
-};
+  // Methode pour changer de mois avec la flèche gauche
+  function previousMonth() {
+    if (selectedMonth.value === 0) {
+      selectedMonth.value = 11;
+      selectedYear.value--;
+    } else {
+      selectedMonth.value--;
+    }
+  }
+
+  // Methode pour changer de mois avec la flèche droite
+function nextMonth() {
+  if (selectedMonth.value === 11) {
+    selectedMonth.value = 0;
+    selectedYear.value++;
+  } else {
+    selectedMonth.value++;
+  }
+}
+
 </script>
-
-  
--- src/pages/dashboard/home-ri.vue
+++ src/pages/dashboard/home-ri.vue
@@ -1,7 +1,21 @@
 <template>
-    ri
+    <div v-if="events && events.count > 0">
+        <CalendarComp :events="events"></CalendarComp>
+        {{ events }}
+    </div>
 </template>
 
 <script setup>
+    import CalendarComp from '../../components/utils/CalendarComp.vue';
+    import { onMounted, ref } from 'vue'
+    import { request } from '../../composables/httpRequest';
+    import config from '../../config';
 
+    const events = ref([])
+
+    async function fetch(){
+        await request('GET', false, events, config.apiUrl+'api/event')
+    }
+
+    onMounted(fetch)
 </script>
