46147e890c894e8b72d2957d045bf8704b8cb687
Akinaru <maxime.utchimata@gmail.com>
2024-05-25 18:55:54 +0200

    invisible visible departement sur accord

--- src/pages/dashboard/accord.vue
+++ src/pages/dashboard/accord.vue
@@ -112,10 +112,10 @@
                             <p>Les départements: </p>
                             <div class="flex items-center justify-start">
                                 <div v-for="(dept, indexDept) in accord.departments" :key="indexDept">
-                                    <div class="w-fit p-2 flex drop-shadow-lg items-center justify-center mx-1 tooltip select-none font-bold" :data-tip="'Département '+ dept.dept_name" :style="{backgroundColor: dept.dept_color}">
-                                        <p>{{ dept.dept_shortname }} <span class="font-bold">{{ dept.pivot.deptagree_valide === 0 ? '(Non validé par le dep)' : '' }}</span></p>
-                                        <button class="hover:opacity-60 hover:cursor-pointer bg-base-300 flex items-center justify-center p-1 ml-2" >
-                                            <svg class="stroke-current shrink-0 h-5 w-5" fill="#000000" height="24px" width="24px" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" 
+                                    <div class="w-fit p-2 flex drop-shadow-lg items-center justify-center mx-1 tooltip select-none font-bold" :data-tip="(dept.pivot.deptagree_valide == 0 ? '(INVISIBLE) ' : '')+'Département '+ dept.dept_name" :style="{backgroundColor: dept.dept_color}" >
+                                        <p>{{ dept.dept_shortname }}<span class="font-bold">{{ dept.pivot.deptagree_valide === 0 ? ' (Invisible)' : '' }}</span></p>
+                                        <button  class="hover:opacity-60 hover:cursor-pointer bg-base-300 flex items-center justify-center p-1 ml-2" @click="changeVisibility(accord.agree_id, dept.dept_id, dept.pivot.deptagree_valide)">
+                                            <svg v-if="dept.pivot.deptagree_valide === 1" class="stroke-current shrink-0 h-5 w-5" fill="#000000" height="24px" width="24px" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" 
                                                     viewBox="0 0 488.85 488.85" xml:space="preserve">
                                                 <g>
                                                     <path d="M244.425,98.725c-93.4,0-178.1,51.1-240.6,134.1c-5.1,6.8-5.1,16.3,0,23.1c62.5,83.1,147.2,134.2,240.6,134.2
@@ -124,7 +124,20 @@
                                                         C343.725,302.225,302.225,343.725,251.125,347.025z M248.025,299.625c-33.4,2.1-61-25.4-58.8-58.8c1.7-27.6,24.1-49.9,51.7-51.7
                                                         c33.4-2.1,61,25.4,58.8,58.8C297.925,275.625,275.525,297.925,248.025,299.625z"/>
                                                 </g>
-                                                </svg>
+                                            </svg>
+                                            <svg v-else class="stroke-current shrink-0 h-5 w-5" fill="#ff0000" height="24px" width="24px" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 488.85 488.85" xml:space="preserve">
+                                            <g>
+                                                <!-- Contour de l'œil -->
+                                                <path d="M244.425,98.725c-93.4,0-178.1,51.1-240.6,134.1c-5.1,6.8-5.1,16.3,0,23.1c62.5,83.1,147.2,134.2,240.6,134.2
+                                                    s178.1-51.1,240.6-134.1c5.1-6.8,5.1-16.3,0-23.1C422.525,149.825,337.825,98.725,244.425,98.725z M251.125,347.025
+                                                    c-62,3.9-113.2-47.2-109.3-109.3c3.2-51.2,44.7-92.7,95.9-95.9c62-3.9,113.2,47.2,109.3,109.3
+                                                    C343.725,302.225,302.225,343.725,251.125,347.025z M248.025,299.625c-33.4,2.1-61-25.4-58.8-58.8c1.7-27.6,24.1-49.9,51.7-51.7
+                                                    c33.4-2.1,61,25.4,58.8,58.8C297.925,275.625,275.525,297.925,248.025,299.625z"/>
+                                                <!-- Ligne de barre de l'œil -->
+                                                <line x1="100" y1="100" x2="400" y2="400" style="stroke:red;stroke-width:40"/>
+                                            </g>
+                                        </svg>
+
                                         </button>
                                         <button class="hover:opacity-60 hover:cursor-pointer bg-base-300 flex items-center justify-center p-1 ml-2" @click="removeDeptFromAgreement(accord.agree_id, dept.dept_id)">
                                             <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-5 w-5" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path></svg>
@@ -314,6 +327,23 @@
         newAgreement.value.newuniv.name = '';
         newAgreement.value.newuniv.city = '';
         newAgreement.value.newuniv.newpartnercountry = '';
+    }
+
+    async function changeVisibility(accordId, deptId, deptagree_valide) {
+        const newVisibility = deptagree_valide === 0 ? 1 : 0;
+
+        const requestData = {
+            dept_id: deptId,
+            agree_id: accordId,
+            deptagree_valide: newVisibility
+        }
+
+        await request('PUT', true, response, config.apiUrl+'api/departmentagreement/changevisibility', requestData);
+        if(response.value.status == 200){
+            const accord = accords.value.agreements.find(accord => accord.agree_id == accordId);
+            const dept = accord.departments.find(dept => dept.dept_id == deptId);
+            dept.pivot.deptagree_valide = newVisibility;
+        }
     }
 
 
