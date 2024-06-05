251e945f454ead68b3f5220ab4d72b6fd1dd03d4
Akinaru <maxime.utchimata@gmail.com>
2024-06-05 16:16:44 +0200

    "Added event count display, wrapped title and event list in a div, and moved filteredEvents assignment to fetchEvents function"

--- src/pages/evenementliste.vue
+++ src/pages/evenementliste.vue
@@ -6,21 +6,24 @@
                 <li>Évènements</li> 
             </ul>
         </div>
-        <p class="font-bold text-xl py-10 flex items-center justify-center">{{ displayMode === 'all' ? 'Liste des évènements' : 'Évènements du ' + formatDate(selectedDate) }}</p>
-        <div v-if="filteredEvents && filteredEvents.length > 0" class="flex flex-col justify-center items-center">
-            <RouterLink v-for="event in filteredEvents" :key="event.evt_id" class="hover:opacity-60 sm:w-2/3 w-full bg-base-300 my-2 flex overflow-hidden" :to="{name: 'EvenementDetail', params: {evt_id: event.evt_id}}">
-                <p class="font-bold md:text-xl flex items-center justify-center md:p-5 p-2 transition-all duration-200 ease-in-out">{{ formatDate(event.evt_datetime) }}</p>
-                <div class="flex flex-col w-5/6 items-start justify-center md:py-5 p-3">
-                    <div class="flex items-start justify-start w-full md:flex-row flex-col transition-all duration-200 ease-in-out">
-                        <span class="badge badge-warning min-w-fit transition-all duration-200 ease-in-out">{{ event.theme.evthm_name }}</span>
-                        <p class="font-bold md:mx-3 transition-all duration-200 ease-in-out">{{ event.evt_name }}</p>
+        <div>
+            <p class="font-bold text-xl py-10 flex items-center justify-center">{{ displayMode === 'all' ? 'Liste des évènements' : 'Évènements du ' + formatDate(selectedDate) }}</p>
+            <p class="flex items-center justify-center">{{ filteredEvents.length }} résultat{{ filteredEvents.length > 1 ? 's' : '' }}</p>
+            <div v-if="filteredEvents && filteredEvents.length > 0" class="flex flex-col justify-center items-center">
+                <RouterLink v-for="event in filteredEvents" :key="event.evt_id" class="hover:opacity-60 sm:w-2/3 w-full bg-base-300 my-2 flex overflow-hidden" :to="{name: 'EvenementDetail', params: {evt_id: event.evt_id}}">
+                    <p class="font-bold md:text-xl flex items-center justify-center md:p-5 p-2 transition-all duration-200 ease-in-out">{{ formatDate(event.evt_datetime) }}</p>
+                    <div class="flex flex-col w-5/6 items-start justify-center md:py-5 p-3">
+                        <div class="flex items-start justify-start w-full md:flex-row flex-col transition-all duration-200 ease-in-out">
+                            <span class="badge badge-warning min-w-fit transition-all duration-200 ease-in-out">{{ event.theme.evthm_name }}</span>
+                            <p class="font-bold md:mx-3 transition-all duration-200 ease-in-out">{{ event.evt_name }}</p>
+                        </div>
+                        <p class="truncate overflow-hidden w-full whitespace-nowrap transition-all duration-200 ease-in-out">{{ event.evt_description }}</p>
                     </div>
-                    <p class="truncate overflow-hidden w-full whitespace-nowrap transition-all duration-200 ease-in-out">{{ event.evt_description }}</p>
-                </div>
-            </RouterLink>
-        </div>
-        <div v-else class="flex items-center justify-center p-52">
-            <p class="text-lg">{{ displayMode === 'all' ? 'Aucun évènement...' : 'Aucun évènement pour cette journée...' }}</p>
+                </RouterLink>
+            </div>
+            <div v-else class="flex items-center justify-center p-52">
+                <p class="text-lg">{{ displayMode === 'all' ? 'Aucun évènement...' : 'Aucun évènement pour cette journée...' }}</p>
+            </div>
         </div>
     </div>
 </template>
@@ -47,6 +50,7 @@ function formatDate(date) {
 async function fetchEvents() {
     isLoaded.value = false;
     await request('GET', false, events, config.apiUrl + 'api/event');
+    filteredEvents.value = events.value.events;
     isLoaded.value = true;
 }
 
