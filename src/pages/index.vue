96e3bfbadf7a261d749fcbd3c193593c669702f2
Akinaru <maxime.utchimata@gmail.com>
2024-06-10 00:49:42 +0200

    articles

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -116,42 +116,40 @@
                 <p class="text-xl font-bold">Articles</p>
                     <div v-if="articles && articles.articles" class="flex justify-center">
 
-                        <div class="flex flex-wrap justify-center w-full" v-if="articles.count > 0">
-    <RouterLink v-for="(article, index) in articles.articles" :key="index" :to="{name: 'Article', params: {art_id: article.art_id}}">
-        <div class="bg-base-300 p-4 hover:opacity-80 drop-shadow-lg hover:scale-105 scale-100 transition-all duration-100 ease-in-out max-w-sm w-full sm:max-w-md md:max-w-lg lg:max-w-xl xl:max-w-2xl mx-2 mb-4 flex h-full">
+                        <!-- <div class="flex flex-wrap justify-center w-full" v-if="articles.count > 0">
+                            <RouterLink v-for="(article, index) in articles.articles" :key="index" :to="{name: 'Article', params: {art_id: article.art_id}}">
+                                <div class="bg-base-300 p-4 hover:opacity-80 drop-shadow-lg hover:scale-105 transition-all duration-100 ease-in-out max-w-sm w-full sm:max-w-md md:max-w-lg lg:max-w-xl xl:max-w-2xl mx-2 mb-4 flex flex-col h-80">
+                                    <div class="w-full h-40 overflow-hidden">
+                                        <img :src="article.art_image ? config.apiUrl+'api/article/image/'+article.art_id : 'https://envie-sante.fr/img/p/fr-default-large_default.jpg'" alt="Image" class="object-cover h-full w-full">
+                                    </div>
+                                    <div class="flex flex-col justify-between flex-grow p-2">
+                                        <div>
+                                            <div class="flex items-center mb-2">
+                                                <span v-if="article.art_pin" class="badge badge-primary mx-2">Épinglé</span>
+                                                <p class="text-lg font-bold">{{ article.art_title }}</p>
+                                            </div>
+                                            <p class="text-sm text-gray-600 mb-2">Dernière modif: {{ article.art_lastmodif }}</p>
+                                        </div>
+                                        <p class="overflow-hidden text-sm" style="max-height: 4rem;">{{ article.art_description }}</p>
+                                    </div>
+                                </div>
+                            </RouterLink>
+                        </div> -->
 
-            <div class="w-1/3 mr-4 flex items-center justify-center max-h-full overflow-hidden" style="min-width: 200px;">
-                <img :src="article.art_image ? config.apiUrl+'api/article/image/'+article.art_id : 'https://envie-sante.fr/img/p/fr-default-large_default.jpg'" alt="Image" class="object-cover h-full w-full" style="max-height: 200px;">
+
+                        <div class="flex flex-wrap justify-center w-full bg-red-105 gap-5" v-if="articles.count > 0">
+    <RouterLink v-for="(article, index) in articles.articles" :key="index" :to="{name: 'Article', params: {art_id: article.art_id}}" class="relative bg-base-300 w-80 md:w-110 h-96 transition-all duration-100 ease-in-out drop-shadow-lg hover:scale-105">
+        <div :style="{ backgroundImage: `url(${article.art_image ? config.apiUrl + 'api/article/image/' + article.art_id : 'https://envie-sante.fr/img/p/fr-default-large_default.jpg'})` }" class="bg-cover bg-center w-full h-48"></div>
+        <span v-if="article.art_pin" class="badge badge-primary absolute top-1 left-1">📌Épinglé</span>
+        <div class="p-4 flex flex-col justify-start h-52">
+            <div class="mb-2">
+                <p class="font-bold text-xl">{{ article.art_title }}</p>
+                <p class="text-gray-600 text-sm">Dernière modif: {{ article.art_lastmodif }}</p>
             </div>
-            
-            <div class="flex flex-col justify-start w-2/3">
-                <div style="min-width: 0;">
-                    <div class="flex items-center">
-                        <span v-if="article.art_pin" class="badge badge-primary mx-2">Épinglé</span>
-                        <p class="text-lg font-bold">{{ article.art_title }}</p>
-                    </div>
-                    <p>Dernière modif: {{ article.art_lastmodif }}</p>
-                </div>
-                <div class="overflow-hidden max-h-36 min-h-36 my-2">
-                    <p class="overflow-hidden max-h-full">{{ article.art_description }}</p>
-                </div>
-            </div>
+            <p class="overflow-hidden text-sm text-gray-700 max-h-24">{{ article.art_description }}{{ article.art_description }}</p>
         </div>
     </RouterLink>
 </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
