b743ae6aef388519c242edc73887a31f0bbdcaad
Akinaru <maxime.utchimata@gmail.com>
2024-10-07 10:38:06 +0200

    fix connexion

--- src/router.js
+++ src/router.js
@@ -6,28 +6,7 @@ import { ref } from 'vue';
 import config from './config';
 import { request } from './composables/httpRequest';
 
-const requireAuth = (to, from, next) => {
-    const accountStore = useAccountStore();
-    
-    if (accountStore.isLogged()) {
-        if(accountStore.getAccessLevel() > 0){
-            next();
-        }else{
-            if(accountStore.getAccountValidate()){
-                next();
-            }else{
-                next({name: 'ComplDossier'})
-            }
-        }
-    } else {
-        next({ name: 'Login' });
-        addAlert('error', {
-            data: { 
-                error: 'Vous devez être connecté pour accéder à cette page.'
-            }
-        })
-    }
-};
+
 
 const requireAccess = (accessLevel) => (to, from, next) => {
     const accountStore = useAccountStore();
@@ -70,31 +49,27 @@ const isAlreadyComplete = (to, from, next) => {
 }
 
 const routes = [
-    { path: '/', name: 'Accueil', component: Index, meta: { title: 'Accueil' } },
-    { path: '/not-found', name: 'NotFound', component: () => import('./pages/notfound.vue'), meta: { title: 'Not Found' } },
+    { path: '/', name: 'Accueil', component: Index, meta: { title: 'Accueil', requiresAuth: false } },
+    { path: '/not-found', name: 'NotFound', component: () => import('./pages/notfound.vue'), meta: { title: 'Not Found', requiresAuth: false } },
     { path: '/:pathMatch(.*)*', redirect: '/not-found' },
-    { path: '/login', name: 'Login', component: () => import('./pages/login.vue'), beforeEnter: isAlreadyLogin, meta: { title: 'Login' } },
-    { path: '/compldossier', name: 'ComplDossier', component: () => import('./pages/compldossier.vue'), beforeEnter: isAlreadyComplete, meta: { title: 'Compléter Dossier' } },
-    { path: '/articles', name: 'Articles', component: () => import('./pages/articles.vue'), meta: { title: 'Liste des articles' } },
-    { path: '/article/:art_id', name: 'Article', component: () => import('./pages/article.vue'), meta: {
-        title: route => `Article n° ${route.params.art_id}`
-    } },
-    { path: '/accord/:agree_id', name: 'Accord', component: () => import('./pages/accord.vue'), meta: {
-        title: route => `Accord n° ${route.params.agree_id}`
-    } },
-    { path: '/evenement', name: 'Evenement', component: () => import('./pages/evenementliste.vue'), meta: { title: 'Liste des évènements' } },
-    { path: '/evenement/:evt_id', name: 'EvenementDetail', component: () => import('./pages/evenementdetail.vue'), meta: { title: route => `Évènement ${route.params.evt_id}` }},
+    { path: '/login', name: 'Login', component: () => import('./pages/login.vue'), beforeEnter: isAlreadyLogin, meta: { title: 'Login', requiresAuth: false } },
+    { path: '/compldossier', name: 'ComplDossier', component: () => import('./pages/compldossier.vue'), beforeEnter: isAlreadyComplete, meta: { title: 'Compléter Dossier', requiresAuth: true } },
+    { path: '/articles', name: 'Articles', component: () => import('./pages/articles.vue'), meta: { title: 'Liste des articles', requiresAuth: true } },
+    { path: '/article/:art_id', name: 'Article', component: () => import('./pages/article.vue'), meta: { title: route => `Article n° ${route.params.art_id}`, requiresAuth: true } },
+    { path: '/accord/:agree_id', name: 'Accord', component: () => import('./pages/accord.vue'), meta: { title: route => `Accord n° ${route.params.agree_id}`, requiresAuth: true } },
+    { path: '/evenement', name: 'Evenement', component: () => import('./pages/evenementliste.vue'), meta: { title: 'Liste des évènements', requiresAuth: true } },
+    { path: '/evenement/:evt_id', name: 'EvenementDetail', component: () => import('./pages/evenementdetail.vue'), meta: { title: route => `Évènement ${route.params.evt_id}`, requiresAuth: true } },
 
     { 
         path: '/dashboard', 
         component: () => import('./pages/dashboard.vue'), 
-        beforeEnter: requireAuth,
+        meta: { title: 'Dashboard', requiresAuth: true },
         children: [
-            { path: '', name: 'Dashboard', meta: { title: 'Dashboard' }, beforeEnter: (to, from, next) => {
+            { path: '', name: 'Dashboard', meta: { title: 'Dashboard', requiresAuth: true }, beforeEnter: (to, from, next) => {
                 const accountStore = useAccountStore();
-                if (accountStore.getAccessLevel() && accountStore.getAccessLevel() === 1) {
+                if (accountStore.getAccessLevel() === 1) {
                     next({ name: 'HomeRI' });
-                } else if (accountStore.getAccessLevel() && accountStore.getAccessLevel() === 2) {
+                } else if (accountStore.getAccessLevel() === 2) {
                     next({ name: 'HomeDept' });
                 } else {
                     next({ name: 'Home' });
@@ -105,111 +80,110 @@ const routes = [
                 name: 'ArticleDash', 
                 component: () => import('./pages/dashboard/article.vue'),
                 beforeEnter: requireAccess(1),
-                meta: { title: 'Dashboard - Article' }
+                meta: { title: 'Dashboard - Article', requiresAuth: true }
             },
             { 
                 path: 'departement', 
                 name: 'DepartementDash', 
                 component: () => import('./pages/dashboard/departement.vue'),
                 beforeEnter: requireAccess(1),
-                meta: { title: 'Dashboard - Departement' }
+                meta: { title: 'Dashboard - Departement', requiresAuth: true }
             },
             { 
                 path: 'universite', 
                 name: 'UniversiteDash', 
                 component: () => import('./pages/dashboard/universite.vue'),
                 beforeEnter: requireAccess(1),
-                meta: { title: 'Dashboard - Université' } 
+                meta: { title: 'Dashboard - Université', requiresAuth: true } 
             },
             { 
                 path: 'isced', 
                 name: 'IscedDash', 
                 component: () => import('./pages/dashboard/isced.vue'),
                 beforeEnter: requireAccess(1),
-                meta: { title: 'Dashboard - ISCED' } 
+                meta: { title: 'Dashboard - ISCED', requiresAuth: true } 
             },
             { 
                 path: 'accord', 
                 name: 'AccordDash', 
                 component: () => import('./pages/dashboard/accord.vue'),
                 beforeEnter: requireAccess(1),
-                meta: { title: 'Dashboard - Accord' }
+                meta: { title: 'Dashboard - Accord', requiresAuth: true }
             },
             { 
                 path: 'access', 
                 name: 'AccessDash', 
                 component: () => import('./pages/dashboard/access.vue'),
                 beforeEnter: requireAccess(1),
-                meta: { title: 'Dashboard - Access' }
+                meta: { title: 'Dashboard - Access', requiresAuth: true }
             },
             { 
                 path: 'etudiants', 
                 name: 'EtudiantsDash', 
                 component: () => import('./pages/dashboard/etudiants.vue'),
                 beforeEnter: checkMultipleAccess(1, 2),
-                meta: { title: 'Dashboard - Etudiants' }
+                meta: { title: 'Dashboard - Etudiants', requiresAuth: true }
             },
             { 
                 path: 'historique', 
                 name: 'HistoriqueDash', 
                 component: () => import('./pages/dashboard/historique.vue'),
                 beforeEnter: requireAccess(1),
-                meta: { title: 'Dashboard - Historique' }
+                meta: { title: 'Dashboard - Historique', requiresAuth: true }
             },
             { 
                 path: 'evenement', 
                 name: 'EvenementDash', 
                 component: () => import('./pages/dashboard/evenement.vue'),
                 beforeEnter: requireAccess(1),
-                meta: { title: 'Dashboard - Evenements' }
+                meta: { title: 'Dashboard - Evenements', requiresAuth: true }
             },
             { 
                 path: 'documents', 
                 name: 'DocumentsDash', 
                 component: () => import('./pages/dashboard/documents.vue'),
                 beforeEnter: requireAccess(1),
-                meta: { title: 'Dashboard - Documents' }
+                meta: { title: 'Dashboard - Documents', requiresAuth: true }
             },
             { 
                 path: 'arbitrage', 
                 name: 'Arbitrage', 
                 component: () => import('./pages/dashboard/arbitrage.vue'),
                 beforeEnter: requireAccess(1),
-                meta: { title: 'Dashboard - Arbitrage' }
+                meta: { title: 'Dashboard - Arbitrage', requiresAuth: true }
             },
             { 
                 path: 'home-ri', 
                 name: 'HomeRI', 
                 component: () => import('./pages/dashboard/home-ri.vue'),
                 beforeEnter: requireAccess(1),
-                meta: { title: 'Dashboard - Home RI' }
+                meta: { title: 'Dashboard - Home RI', requiresAuth: true }
             },
             { 
                 path: 'home-dept', 
                 name: 'HomeDept', 
                 component: () => import('./pages/dashboard/home-dept.vue'),
                 beforeEnter: requireAccess(2),
-                meta: { title: 'Dashboard - Home Dept' }
+                meta: { title: 'Dashboard - Home Dept', requiresAuth: true }
             },
             { 
                 path: 'home', 
                 name: 'Home', 
                 component: () => import('./pages/dashboard/home.vue'),
                 beforeEnter: requireAccess(0),
-                meta: { title: 'Dashboard - Home' }
+                meta: { title: 'Dashboard - Home', requiresAuth: true }
             },
             { 
                 path: 'profile/:acc_id', 
                 name: 'Profile', 
                 component: () => import('./pages/dashboard/profile.vue'),
                 beforeEnter: checkMultipleAccess(1, 2),
-                meta: {
-                    title: route => `Dashboard - Profile de ${route.params.acc_id}`
-                }
+                meta: { title: route => `Dashboard - Profile de ${route.params.acc_id}`, requiresAuth: true }
             },
         ]
     },
 ];
+
 
 
 const account = ref([])
@@ -246,31 +220,48 @@ const router = createRouter({
     },
 });
 
-router.isReady().then(async () => {
+router.beforeEach(async (to, from, next) => {
     const accountStore = useAccountStore();
     
-    try {
-        const [isLogged, userLogin] = await checkUserLogin()
-        if(isLogged && userLogin != null){
-          await request('GET', false, account, config.apiUrl+'api/account/getbylogin/'+userLogin);
-          const patchedValues = {
-            login: account.value.acc_id,
-            fullname: account.value.acc_fullname,
-            logged: true,
-            last_login: new Date().toISOString(),
-            access: account.value.access ? account.value.access.acs_accounttype : 0,
-            acc_validateacc: account.acc_validateacc
-          }
-          accountStore.$patch(patchedValues); // Utilisez uniquement les propriétés nécessaires
-        }
-    } catch (error) {
-        console.error('Erreur lors de la vérification de la connexion :', error);
-    }
-});
+    // Vérifiez si l'utilisateur est connecté
+    const [isLogged, userLogin] = await checkUserLogin();
 
-router.beforeEach((to, from, next) => {
+    // Si la route nécessite une authentification
+    if (to.matched.some(record => record.meta.requiresAuth)) {
+        if (!isLogged) {
+            next({ name: 'Accueil' });
+            addAlert('error', {
+                data: { error: 'Vous devez être connecté pour accéder à cette page.' }
+            });
+            return; // Assurez-vous de sortir de la fonction
+        } else {
+            // Mettez à jour le store si l'utilisateur est connecté
+            if (userLogin != null) {
+                await request('GET', false, account, config.apiUrl + 'api/account/getbylogin/' + userLogin);
+                
+                const patchedValues = {
+                    login: account.value.acc_id,
+                    fullname: account.value.acc_fullname,
+                    logged: true,
+                    last_login: new Date().toISOString(),
+                    access: account.value.access ? account.value.access.acs_accounttype : 0,
+                    acc_validateacc: account.value.acc_validateacc
+                };
+                accountStore.$patch(patchedValues);
+                
+                // Vérifiez la validation du compte ici
+                if (accountStore.getAccessLevel() === 0 && !accountStore.getAccountValidate()) {
+                    next({ name: 'ComplDossier' });
+                    return; // Assurez-vous de sortir de la fonction
+                }
+            }
+        }
+    }
+
+    // Mettez à jour le titre de la page
     const title = typeof to.meta.title === 'function' ? to.meta.title(to) : to.meta.title;
     document.title = `ILC - ${title || to.name}`;
+    
     next();
 });
 
