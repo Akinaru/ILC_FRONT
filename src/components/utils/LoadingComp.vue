d91c268ec3053b3b68cf116a54090c72453e281b
Akinaru <maxime.utchimata@gmail.com>
2024-10-23 21:37:35 +0200

    loading

--- src/components/utils/LoadingComp.vue
+++ src/components/utils/LoadingComp.vue
@@ -1,10 +1,33 @@
 <template>
-    <div class="flex items-center justify-center my-20 py-72 flex-col">
-        <p class="p-5 font-bold select-none">Chargement des données en cours...</p>
-        <span class="loading loading-dots loading-lg"></span>
+    <div class="min-h-screen flex items-center justify-center flex-col">
+        <p class="p-3 font-bold select-none sm:text-2xl">Chargement des données en cours...</p>
+        <Vue3Lottie :animationData="loading" :height="animationWidth" :width="animationWidth" />
     </div>
 </template>
 
 <script setup>
+import { Vue3Lottie } from 'vue3-lottie'
+import loading from '../../animations/loading.json'
+import { ref, onMounted, onBeforeUnmount, computed } from 'vue'
 
+const screenWidth = ref(window.innerWidth)
+
+const updateDimensions = () => {
+  screenWidth.value = window.innerWidth
+}
+
+onMounted(() => {
+  window.addEventListener('resize', updateDimensions)
+  // Initial calculation
+  updateDimensions()
+})
+
+onBeforeUnmount(() => {
+  window.removeEventListener('resize', updateDimensions)
+})
+
+const animationWidth = computed(() => {
+  // Ajuste la largeur en fonction de la taille de l'écran, avec une largeur minimale et maximale
+  return Math.min(Math.max(screenWidth.value * 0.6, 200), 500)
+})
 </script>
