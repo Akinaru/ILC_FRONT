84bd929fab409082b89e3ee827ea97860fa38319
Akinaru <maxime.utchimata@gmail.com>
2025-01-06 13:55:08 +0100

    correction token not created at post account

--- src/composables/authGuard.js
+++ src/composables/authGuard.js
@@ -7,32 +7,26 @@ import { addAlert } from '../composables/addAlert';
 
 export async function authLogAccount(login, router) {
     const response = ref([]);
-    await request('GET', false, response, config.apiUrl + 'api/account/getbylogin/' + login);
-    if (response.value && response.value.status == 404) {
-        await authRegisterAccount(login, router);
-    } else {
-        await request('PUT', false, response, config.apiUrl+'api/account/login/'+login)
-        
-        var requestData = {
-            acc_id: response.value.acc_id,
-            acc_fullname: response.value.acc_fullname,
-            acc_lastlogin: response.value.acc_lastlogin,
-            acc_validateacc: response.value.acc_validateacc
-        };
-        await request('GET', false, response, config.apiUrl+'api/access/getbylogin/'+login);
-        if(response.value.count == 1){
-            requestData.acc_access = response.value.access.acs_accounttype;
-        }
-        else{
-            requestData.acc_access = 0;
-        }
-        authStoreUser(requestData);
-        router.push({ name: 'Dashboard' });
-        
+    await request('PUT', false, response, config.apiUrl+'api/account/login/'+login)
+    
+    var requestData = {
+        acc_id: response.value.acc_id,
+        acc_fullname: response.value.acc_fullname,
+        acc_lastlogin: response.value.acc_lastlogin,
+        acc_validateacc: response.value.acc_validateacc
+    };
+    await request('GET', false, response, config.apiUrl+'api/access/getbylogin/'+login);
+    if(response.value.count == 1){
+        requestData.acc_access = response.value.access.acs_accounttype;
     }
+    else{
+        requestData.acc_access = 0;
+    }
+    authStoreUser(requestData);
+    router.push({ name: 'Dashboard' });
 }
 
-async function authRegisterAccount(login, router) {
+export async function authRegisterAccount(login, router) {
     // Récupération des informations de l'utilisateur depuis le LDAP
     const decomposedInfo = ref([]);
     const response = ref([]);
@@ -55,6 +49,9 @@ async function authRegisterAccount(login, router) {
             acc_id: decomposedInfo.value.login,
             acc_fullname: decomposedInfo.value.fullname,
         };
+
+
+
         await request('POST', false, response, config.apiUrl + 'api/account', requestData);
 
         if (response.value.status && response.value.status === 201) {
@@ -66,9 +63,12 @@ async function authRegisterAccount(login, router) {
                 acc_access: response.value.access
             };
 
+            localStorage.setItem('token', response.value.token);
+
             authStoreUser(userData);
             router.push({ name: 'Dashboard' });
         }
+
     } catch (error) {
         console.error("Une erreur est survenue (register_acc_auth):", error);
         // Vous pouvez également afficher un message d'erreur à l'utilisateur ici
--- src/pages/login.vue
+++ src/pages/login.vue
@@ -6,7 +6,7 @@
 
 <script setup>
 import { ref, onMounted } from 'vue';
-import { authLogAccount } from '../composables/authGuard';
+import { authLogAccount, authRegisterAccount } from '../composables/authGuard';
 import { useRouter } from 'vue-router';
 import { request } from '../composables/httpRequest';
 import config from '../config';
@@ -41,20 +41,27 @@ async function loginUser() {
 
         await request('POST', false, response, config.apiUrl + 'api/login', {login: login.value});
 
-        localStorage.setItem('token', response.value.token);
 
-        
-        await request('GET', false, acceptedResponse, config.apiUrl + 'api/acceptedaccount/getbylogin/' + login.value);
-        await request('GET', false, accessResponse, config.apiUrl + 'api/access/getbylogin/' + login.value);
+        if(response.value.status != 404){
+            //Le compte du user existe deja
+            localStorage.setItem('token', response.value.token);
 
-        const isLoginAccepted = acceptedResponse.value && acceptedResponse.value.account;
-        const isLoginAccess = accessResponse.value && accessResponse.value.access;
 
-        if (isLoginAccepted || isLoginAccess) {
-            await authLogAccount(login.value, router);
-        } else {
-            addAlert('error', { data: { error: 'Vous n\'êtes pas autorisé à accéder à la partie connectée.', message: 'Veuillez vous renseigner auprès du service ILC.' } });
-            router.push({ name: 'Accueil' });
+            await request('GET', false, acceptedResponse, config.apiUrl + 'api/acceptedaccount/getbylogin/' + login.value);
+            await request('GET', false, accessResponse, config.apiUrl + 'api/access/getbylogin/' + login.value);
+
+            const isLoginAccepted = acceptedResponse.value && acceptedResponse.value.account;
+            const isLoginAccess = accessResponse.value && accessResponse.value.access;
+
+            if (isLoginAccepted || isLoginAccess) {
+                await authLogAccount(login.value, router);
+            } else {
+                addAlert('error', { data: { error: 'Vous n\'êtes pas autorisé à accéder à la partie connectée.', message: 'Veuillez vous renseigner auprès du service ILC.' } });
+                router.push({ name: 'Accueil' });
+            }
+        }else{
+            //Le compte du user n'existe pas
+            await authRegisterAccount(login.value, router);
         }
     } catch (error) {
         addAlert('error', { data: { error: 'Une erreur s\'est produite lors de la connexion.', message: error.message } });
