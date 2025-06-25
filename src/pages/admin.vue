c968a6d2e24acf9c79383f119f5aa511cd20adcb
Akinaru <maxime.utchimata@gmail.com>
2025-06-25 13:35:41 +0200

    admin.vue

---
+++ src/pages/admin.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container mx-auto py-12">
+    <div class="text-center mb-8">
+      <h1 class="text-3xl font-bold mb-2">Accès Administrateur</h1>
+    </div>
+
+    <div v-if="!isUserLoggedIn" class="flex justify-center">
+      <a
+        :href="`${config.apiUrl}cas.php?redirect=${encodeURIComponent(currentUrl)}&admin=true`"
+        class="btn btn-primary btn-outline rounded-full gap-2"
+      >
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+            d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1" />
+        </svg>
+        Connexion administrateur
+      </a>
+    </div>
+
+    <div v-else class="text-center text-success">
+      <p class="text-lg font-semibold">Tu es déjà connecté !</p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { useAccountStore } from '../stores/accountStore';
+import { storeToRefs } from 'pinia'
+import { computed } from 'vue'
+import config from '../config';
+
+const accountStore = useAccountStore()
+const { logged } = storeToRefs(accountStore)
+
+const isUserLoggedIn = computed(() => logged.value)
+const currentUrl = window.location.href
+</script>
--- src/router.js
+++ src/router.js
@@ -54,6 +54,7 @@ const routes = [
     { path: '/compldossier', name: 'ComplDossier', component: () => import('./pages/compldossier.vue'), beforeEnter: isAlreadyComplete, meta: { title: 'Votre dossier est incomplet!', requiresAuth: false } },
     { path: '/articles', name: 'Articles', component: () => import('./pages/articles.vue'), meta: { title: 'Liste des articles', requiresAuth: false } },
     { path: '/aide', name: 'Aide', component: () => import('./pages/aide.vue'), meta: { title: 'Aide', requiresAuth: false } },
+    { path: '/admin', name: 'Admin', component: () => import('./pages/admin.vue'), meta: { title: 'Admin', requiresAuth: false } },
     { path: '/article/:art_id', name: 'Article', component: () => import('./pages/article.vue'), meta: { title: route => `Article n° ${route.params.art_id}`, requiresAuth: false } },
     { path: '/accord/:agree_id', name: 'Accord', component: () => import('./pages/accord.vue'), meta: { title: route => `Accord n° ${route.params.agree_id}`, requiresAuth: false } },
     { path: '/evenement', name: 'Evenement', component: () => import('./pages/evenementliste.vue'), meta: { title: 'Liste des évènements', requiresAuth: false } },
